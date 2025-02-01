7708920fa5e294197311b2650c14d641fdec422a
Joshua Adesina <140406481+Jayteemighty@users.noreply.github.com>
2025-02-01 14:00:17 +0100

    learn golang

--- 02-Control flow/forloop.go
+++ 02-Control flow/forloop.go
@@ -14,4 +14,17 @@ func main() {
 		fmt.Println("These are the numbers: ", x)
 		x++
 	}
+
+	// RANGE-BASED LOOP
+	// used to iterate over elements in arrays, slices, maps, and strings
+	//syntax:
+	//for index, value  := range collection{
+	// Use index and value
+	//}
+
+	// 1. Iterating Over Arrays and Slices
+	numbers := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 0}
+	for index, value := range numbers {
+		fmt.Println("index: ", index, "value: ", value)
+	}
 }
